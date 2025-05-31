756bd960397b38f08ca3f85e41f67e9a49f4206f
Guilherme Aranega <guilhermee.aranega@gmail.com>
2025-05-31 09:37:45 -0300

    refactor

--- Hospede.java
+++ Hospede.java
@@ -2,9 +2,6 @@ public class Hospede {
     private String nome;
     private String email;
 
-    public Hospede() {
-    }
-
     public Hospede(String nome, String email) {
         this.nome = nome;
         this.email = email;
